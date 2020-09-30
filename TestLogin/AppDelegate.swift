5a19c36a15a0f7c82013230a9999512bde517887
taisei.hikawa <cmmkarubi@gmail.com>
2020-09-30 19:12:37 +0900

    change text of label in conjunction with current user and singing in with first time login user, make document in firestorm

--- TestLogin/AppDelegate.swift
+++ TestLogin/AppDelegate.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
+import FirebaseFirestore
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate ,GIDSignInDelegate{
 
-
-
+    var window: UIWindow?
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
         return true
     }
 
@@ -29,6 +35,46 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        // ...
+        if let error = error {
+            print(error)
+            return
+        }
+        
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                       accessToken: authentication.accessToken)
+        Auth.auth().signIn(with: credential) { (authResult, error) in
+            if let error = error {
+                print("authentication error \(error.localizedDescription)")
+            }else{
+                print("ログイン成功")
+                let db = Firestore.firestore()
+                db.collection("users").document((authResult?.user.uid)!).setData([
+                    "name": "無名",
+                    ]) { error in
+                        if let error = error {
+                            print(error)
+//                            return
+                        }
+                        // 成功したときの処理
+                    print(db.collection("users").document(user.userID).documentID)
+                    }
+                
+            }
+        }
+    }
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+        // Perform any operations when the user disconnects from app here.
+        // ...
+    }
+    
+    @available(iOS 9.0, *)
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any])
+      -> Bool {
+      return GIDSignIn.sharedInstance().handle(url)
     }
 
 
--- TestLogin/ViewController.swift
+++ TestLogin/ViewController.swift
@@ -1,19 +1,52 @@
-//
-//  ViewController.swift
-//  TestLogin
-//
-//  Created by Sakurako Shimbori on 2020/09/29.
-//
-
 import UIKit
+import Firebase
+import GoogleSignIn
+import FirebaseFirestore
 
 class ViewController: UIViewController {
-
+    var handle: AuthStateDidChangeListenerHandle!
+    let db = Firestore.firestore()
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var logOutButton: UIButton!
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var googleSignInButton: GIDSignInButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        GIDSignIn.sharedInstance().signIn()
+        logOutButton.addTarget(self, action: #selector(self.tapLogOutButton(_:)), for: .touchUpInside)
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            if user != nil {
+                //ログインの状態
+                print(Auth.auth().currentUser!)
+                print(user?.uid ?? "nodata")
+                self.logOutButton.isHidden = false
+                self.googleSignInButton.isHidden = true
+                self.db.collection("users").document(user!.uid).getDocument{(document,error) in
+                    if let document = document{
+                        self.userNameLabel.text = document.data()!["name"] as? String
+                    }else{
+                        print(error!)
+                    }
+                }
+            } else {
+                //ログアウトの状態
+                print("nouser")
+                self.logOutButton.isHidden = true
+                self.googleSignInButton.isHidden = false
+                self.userImage.image = UIImage(named: "noimage")
+                self.userNameLabel.text = "no user"
+            }
+        }
     }
-
-
+    
+    @objc func tapLogOutButton(_ sender: UIButton){
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
 }
 
